3675d4f3a6ee22e22e3fe74a55f1f0d6d0b8d209
Millett <nem41@dbmi-dt-036.dbmi.pitt.edu>
2016-02-09 16:03:37 -0500

    initial commit

---
+++ query_service.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import web
+import requests
+
+urls = (
+    '/query/(.*)', 'run_query'
+)
+
+app = web.application(urls, globals())
+
+class run_query:
+    def POST(self, run_id):
+        data = web.input()
+        username = data.username
+        password = data.password
+
+        # first need to get scos message
+        # contact run manager service
+        r = requests.get('http://localhost:8080/run-manager-service-rest-frontend-4.0-SNAPSHOT/ws/run/' \
+                         + run_id + '/files?username=' + username + '&password=' + password)
+        
+
+	return run_id
+
+if __name__ == "__main__":
+    app.run()
